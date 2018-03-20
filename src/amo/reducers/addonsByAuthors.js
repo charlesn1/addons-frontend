09446e31345dc758fbd59ac61a57037c57948af8
Matthew Riley MacPherson (tofumatt) <hi@tofumatt.com>
2018-03-20 19:22:39 +0000

    chore: Allow add-ons to be added to reducer instead of reset

--- src/amo/reducers/addonsByAuthors.js
+++ src/amo/reducers/addonsByAuthors.js
@@ -8,7 +8,7 @@ import type {
 } from 'core/types/addons';
 
 
-type State = {
+export type AddonsByAuthors = {|
   // TODO: It might be nice to eventually stop storing add-ons in this
   // reducer at all and rely on the add-ons in the `addons` reducer.
   // That said, these are partial add-ons returned from the search
@@ -18,9 +18,9 @@ type State = {
   byAddonSlug: { [string]: Array<number> },
   byUserId: { [number]: Array<number> },
   byUsername: { [string]: Array<number> },
-};
+|};
 
-export const initialState: State = {
+export const initialState: AddonsByAuthors = {
   byAddonId: {},
   byAddonSlug: {},
   byUserId: {},
@@ -37,7 +37,7 @@ export const LOAD_ADDONS_BY_AUTHORS: 'LOAD_ADDONS_BY_AUTHORS'
   = 'LOAD_ADDONS_BY_AUTHORS';
 
 type FetchAddonsByAuthorsParams = {|
-  addonType: string,
+  addonType?: string,
   authors: Array<string>,
   errorHandlerId: string,
   forAddonSlug?: string,
@@ -52,7 +52,7 @@ export const fetchAddonsByAuthors = (
   { addonType, authors, errorHandlerId, forAddonSlug }: FetchAddonsByAuthorsParams
 ): FetchAddonsByAuthorsAction => {
   invariant(errorHandlerId, 'An errorHandlerId is required');
-  invariant(addonType, 'An add-on type is required.');
+  // invariant(addonType, 'An add-on type is required.');
   invariant(authors, 'Authors are required.');
   invariant(Array.isArray(authors), 'The authors parameter must be an array.');
 
@@ -88,7 +88,10 @@ export const loadAddonsByAuthors = (
   };
 };
 
-export const getAddonsForSlug = (state: State, slug: string) => {
+export const getAddonsForSlug = (
+  state: AddonsByAuthors,
+  slug: string,
+) => {
   const ids = state.byAddonSlug[slug];
 
   return ids ? ids.map((id) => {
@@ -101,9 +104,9 @@ type Action =
   | LoadAddonsByAuthorsAction;
 
 const reducer = (
-  state: State = initialState,
+  state: AddonsByAuthors = initialState,
   action: Action
-): State => {
+): AddonsByAuthors => {
   switch (action.type) {
     case FETCH_ADDONS_BY_AUTHORS: {
       const newState = deepcopy(state);
@@ -113,13 +116,6 @@ const reducer = (
           ...newState.byAddonSlug,
           [action.payload.forAddonSlug]: undefined,
         };
-      }
-
-      // Reset the data for each author requested.
-      for (const authorUsername of action.payload.authors) {
-        // TODO: Reset the userId here too.
-        // See: https://github.com/mozilla/addons-frontend/issues/4602
-        newState.byUsername[authorUsername] = undefined;
       }
 
       return newState;
