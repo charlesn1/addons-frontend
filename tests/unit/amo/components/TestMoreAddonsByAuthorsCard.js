fc4358f5a7d1bf03c6c642f15d192cb90b02662b
Matthew Riley MacPherson (tofumatt) <hi@tofumatt.com>
2018-03-28 14:20:26 +0100

    Fix lint

--- tests/unit/amo/components/TestMoreAddonsByAuthorsCard.js
+++ tests/unit/amo/components/TestMoreAddonsByAuthorsCard.js
@@ -1,4 +1,3 @@
-import { mount } from 'enzyme';
 import * as React from 'react';
 
 import MoreAddonsByAuthorsCard, {
@@ -6,7 +5,6 @@ import MoreAddonsByAuthorsCard, {
 } from 'amo/components/MoreAddonsByAuthorsCard';
 import {
   fetchAddonsByAuthors,
-  getAddonsForUsernames,
   loadAddonsByAuthors,
 } from 'amo/reducers/addonsByAuthors';
 import {
@@ -16,11 +14,9 @@ import {
   ADDON_TYPE_OPENSEARCH,
   ADDON_TYPE_THEME,
 } from 'core/constants';
-import { createInternalAddon } from 'core/reducers/addons';
 import AddonsCard from 'amo/components/AddonsCard';
 import {
   dispatchClientMetadata,
-  dispatchSearchResults,
   fakeAddon,
   fakeAuthor,
 } from 'tests/unit/amo/helpers';
@@ -32,8 +28,6 @@ import {
 
 
 describe(__filename, () => {
-  let _store;
-
   function fakeAddons() {
     const firstAddon = {
       ...fakeAddon,
@@ -95,20 +89,6 @@ describe(__filename, () => {
       MoreAddonsByAuthorsCardBase
     );
   }
-
-  // function renderAndMount(customProps = {}) {
-  //   const props = {
-  //     store: dispatchClientMetadata().store,
-  //     ...customProps,
-  //   };
-
-  //   return mount(
-  //     <MoreAddonsByAuthorsCard
-  //       i18n={fakeI18n()}
-  //       {...props}
-  //     />
-  //   );
-  // }
 
   it('should render a card', () => {
     const { store } = dispatchClientMetadata();
@@ -429,210 +409,4 @@ describe(__filename, () => {
     expect(root.find(AddonsCard))
       .toHaveProp('header', 'More add-ons by these developers');
   });
-
-  // it('should render "searching" while loading without query', () => {
-  //   _searchStart({ filters: {} });
-  //   const root = render();
-
-  //   expect(root.find('.SearchContextCard-header'))
-  //     .toIncludeText('Loading add-ons');
-  // });
-
-  // it('should render during a search that is loading', () => {
-  //   _searchStart({ filters: { query: 'test' } });
-  //   const root = render();
-
-  //   expect(root.find('.SearchContextCard-header'))
-  //     .toIncludeText('Searching for "test"');
-  // });
-
-  // it('should render search results', () => {
-  //   const { store } = dispatchSearchResults();
-  //   const root = render({ store });
-
-  //   expect(root.find('.SearchContextCard-header'))
-  //     .toIncludeText('2 results for "test"');
-  // });
-
-  // it('should render results that lack a query', () => {
-  //   const { store } = dispatchSearchResults({ filters: {} });
-  //   const root = render({ store });
-
-  //   expect(root.find('.SearchContextCard-header'))
-  //     .toIncludeText('2 add-ons found');
-  // });
-
-  // it('should use singular form when only one result is found', () => {
-  //   const { store } = dispatchSearchResults({
-  //     addons: { [fakeAddon.slug]: fakeAddon },
-  //   });
-  //   const root = render({ store });
-
-  //   expect(root.find('.SearchContextCard-header'))
-  //     .toIncludeText('1 result for "test"');
-  // });
-
-  // it('should use singular form without query when only one result', () => {
-  //   const { store } = dispatchSearchResults({
-  //     addons: { [fakeAddon.slug]: fakeAddon },
-  //     filters: {},
-  //   });
-  //   const root = render({ store });
-
-  //   expect(root.find('.SearchContextCard-header'))
-  //     .toIncludeText('1 add-on found');
-  // });
-
-  // it('should render empty results', () => {
-  //   const { store } = dispatchSearchResults({ addons: [], filters: {} });
-  //   const root = render({ store });
-
-  //   expect(root.find('.SearchContextCard-header'))
-  //     .toIncludeText('No add-ons found');
-  // });
-
-  // it('should render singular form when only one result is found with addonType ADDON_TYPE_THEME', () => {
-  //   const { store } = dispatchSearchResults({
-  //     addons: { [fakeAddon.slug]: fakeAddon },
-  //     filters: {
-  //       addonType: ADDON_TYPE_THEME,
-  //       query: 'test',
-  //     },
-  //   });
-
-  //   const root = render({ store });
-
-  //   expect(root.find('.SearchContextCard-header'))
-  //     .toIncludeText('1 theme found for "test"');
-  // });
-
-  // it('should render plural form when multiple results are found with addonType ADDON_TYPE_THEME', () => {
-  //   const { store } = dispatchSearchResults({
-  //     filters: {
-  //       addonType: ADDON_TYPE_THEME,
-  //       query: 'test',
-  //     },
-  //   });
-
-  //   const root = render({ store });
-
-  //   expect(root.find('.SearchContextCard-header'))
-  //     .toIncludeText('2 themes found for "test"');
-  // });
-
-  // it('should render singular form when only one result is found with addonType ADDON_TYPE_DICT', () => {
-  //   const { store } = dispatchSearchResults({
-  //     addons: { [fakeAddon.slug]: fakeAddon },
-  //     filters: {
-  //       addonType: ADDON_TYPE_DICT,
-  //       query: 'test',
-  //     },
-  //   });
-
-  //   const root = render({ store });
-
-  //   expect(root.find('.SearchContextCard-header'))
-  //     .toIncludeText('1 dictionary found for "test"');
-  // });
-
-  // it('should render plural form when multiple results are found with addonType ADDON_TYPE_DICT', () => {
-  //   const { store } = dispatchSearchResults({
-  //     filters: {
-  //       addonType: ADDON_TYPE_DICT,
-  //       query: 'test',
-  //     },
-  //   });
-
-  //   const root = render({ store });
-
-  //   expect(root.find('.SearchContextCard-header'))
-  //     .toIncludeText('2 dictionaries found for "test"');
-  // });
-
-  // it('should render singular form when only one result is found with addonType ADDON_TYPE_EXTENSION', () => {
-  //   const { store } = dispatchSearchResults({
-  //     addons: { [fakeAddon.slug]: fakeAddon },
-  //     filters: {
-  //       addonType: ADDON_TYPE_EXTENSION,
-  //       query: 'test',
-  //     },
-  //   });
-
-  //   const root = render({ store });
-
-  //   expect(root.find('.SearchContextCard-header'))
-  //     .toIncludeText('1 extension found for "test"');
-  // });
-
-  // it('should render plural form when multiple results are found with addonType ADDON_TYPE_EXTENSION', () => {
-  //   const { store } = dispatchSearchResults({
-  //     filters: {
-  //       addonType: ADDON_TYPE_EXTENSION,
-  //       query: 'test',
-  //     },
-  //   });
-
-  //   const root = render({ store });
-
-  //   expect(root.find('.SearchContextCard-header'))
-  //     .toIncludeText('2 extensions found for "test"');
-  // });
-
-  // it('should render singular form when only one result is found with addonType ADDON_TYPE_LANG', () => {
-  //   const { store } = dispatchSearchResults({
-  //     addons: { [fakeAddon.slug]: fakeAddon },
-  //     filters: {
-  //       addonType: ADDON_TYPE_LANG,
-  //       query: 'test',
-  //     },
-  //   });
-
-  //   const root = render({ store });
-
-  //   expect(root.find('.SearchContextCard-header'))
-  //     .toIncludeText('1 language pack found for "test"');
-  // });
-
-  // it('should render plural form when multiple results are found with addonType ADDON_TYPE_LANG', () => {
-  //   const { store } = dispatchSearchResults({
-  //     filters: {
-  //       addonType: ADDON_TYPE_LANG,
-  //       query: 'test',
-  //     },
-  //   });
-
-  //   const root = render({ store });
-
-  //   expect(root.find('.SearchContextCard-header'))
-  //     .toIncludeText('2 language packs found for "test"');
-  // });
-
-  // it('should render singular form when only one result is found with addonType theme', () => {
-  //   const { store } = dispatchSearchResults({
-  //     addons: { [fakeAddon.slug]: fakeAddon },
-  //     filters: {
-  //       addonType: 'theme',
-  //       query: 'test',
-  //     },
-  //   });
-
-  //   const root = render({ store });
-
-  //   expect(root.find('.SearchContextCard-header'))
-  //     .toIncludeText('1 result found for "test"');
-  // });
-
-  // it('should render plural form when multiple results are found with addonType theme', () => {
-  //   const { store } = dispatchSearchResults({
-  //     filters: {
-  //       addonType: 'theme',
-  //       query: 'test',
-  //     },
-  //   });
-
-  //   const root = render({ store });
-
-  //   expect(root.find('.SearchContextCard-header'))
-  //     .toIncludeText('2 results found for "test"');
-  // });
 });
